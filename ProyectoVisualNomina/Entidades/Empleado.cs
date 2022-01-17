a50cb2cce04476504d784c47d83f4342c983a428
Diego Patiño <diego_Oescobar@outlook.com>
2022-01-17 09:38:18 -0500

    1

---
+++ ProyectoVisualNomina/Entidades/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVisualNomina.Entidades
+{
+    public class Empleado
+    {
+        public int codEmpleado { get; set; }
+        public String NombreEmp { get; set; }
+        public double Sueldo { get; set; }
+        public int Telefono { get; set; }
+        public String EstadoCivil { get; set; }
+        public String Estado { get; set; }
+
+
+
+    }
+}
