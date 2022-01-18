4c7f5134a4707b14e6a8131787048044e23cb7c2
Diego Patiño <diego_Oescobar@outlook.com>
2022-01-18 07:25:51 -0500

    Nomina

---
+++ ProyectoVisualNomina/Entidades/FondoReserva.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVisualNomina.Entidades
+{
+    public class FondosReserva
+    {
+        public int id { get; set; }
+        public int EmpleadoId { get; set; }
+        public float Porcentaje { get; set; }
+        public float valor { get; set; }
+        public ICollection<Empleado> lista { get; set; }
+    }
+}
---
+++ ProyectoVisualNomina/NominaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoVisualNomina.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVisualNomina
+{
+    public class NominaContext:DbContext
+
+    {
+        public DbSet<DecimoCuarto> DecimoCuartos{ get; set; }
+        public DbSet<DecimoTercero> DecimoTerceros { get; set; }
+        public DbSet<FondoReserva> FondosReserva { get; set; }
+        public DbSet<HorasExt> HorasExts { get; set; }
+        public DbSet<RolPago> RolPagos { get; set; }
+        public DbSet<Vacaciones> Vacaciones { get; set; }
+    }
+}
